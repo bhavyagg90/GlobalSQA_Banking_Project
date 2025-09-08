5b80b02d0d1f5d497e6ef8d81f20ce998c47fb26
bhavyagg90 <gbhavya18sliet@gmail.com>
2025-09-08 15:58:04 +0530

    Merge branch 'main' of https://github.com/bhavyagg90/GlobalSQA_Banking_Project.git into main

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -1,18 +1,3 @@
-//package runners;
-//
-//import io.cucumber.testng.AbstractTestNGCucumberTests;
-//import io.cucumber.testng.CucumberOptions;
-//
-//@CucumberOptions(
-//     features = "src/test/resources/features",
-//     glue = "com/globalsqa/banking/stepdefinitions",
-//     plugin = {"pretty", "html:target/cucumber-reports.html"},
-//     tags = "not @ignore"
-//)
-//public class TestRunner extends AbstractTestNGCucumberTests {
-//}
-
-
 package runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
@@ -20,8 +5,6 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
      features = "src/test/resources/features",
-     // 👇 This is the critical change.
-     // It now points to both packages.
      glue = {"stepdefinitions", "hooks"},
      plugin = {"pretty", "html:target/cucumber-reports.html"},
      tags = "not @ignore"
